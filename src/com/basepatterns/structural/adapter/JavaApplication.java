885e800ca1700a73b0c8c495e34197ac735cf5a4
Евгения <ebogdanova@nobilis.team>
2023-06-18 12:55:33 +0200

    implementation of the pattern Adapter

---
+++ src/com/basepatterns/structural/adapter/JavaApplication.java
@@ -0,0 +1,19 @@
+package com.basepatterns.structural.adapter;
+
+public class JavaApplication {
+    public void saveObject() {
+        System.out.println("Saving Java Object...");
+    }
+
+    public void updateObject() {
+        System.out.println("Updating Java Object...");
+    }
+
+    public void loadObject() {
+        System.out.println("Loading Java Object...");
+    }
+
+    public void deleteObject() {
+        System.out.println("Deleting Java Object...");
+    }
+}
